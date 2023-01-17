b08c6244af775d27db3a7ef68c6e212ced01d264
Moustapha DIOP <moustephdiop@gmail.com>
2023-01-17 19:21:42 +0100

    (add) test function

---
+++ src/helpers/bbox.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+
+colors = ["blue", "green", "cyan", "red", "yellow", "magenta", "peru", "azure", "slateblue", "plum"]
+colors = [plt.cm.colors.to_rgb(c) for c in colors]
+
+def display_bbox(image: np.array, bbox: np.array, print_result: bool = False):
+    """Plot the bounding boxes on the image.
+
+    Args:
+        image (np.array): Image to plot.
+        bbox (np.array): Bounding boxes to plot.
+        print_result (bool, optional): Print the result?. Defaults to False.
+    """
+
+    image = image.copy()
+    image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    thick = int((300 + 300) // 900)
+
+    for b in bbox:
+        x1, y1, x2, y2 = b[:4]
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        color = colors[int(b[-1])]
+        
+        if print_result:
+            print(f"bbox: {x1, y1, x2, y2}, class: {int(b[-1])}")
+        
+        cv2.rectangle(image, (x1, y1), (x2, y2), color, thick)
+        cv2.putText(image, str(int(b[-1])), (x1, y1 - 3), 0, 1e-3 * 300, color, thick//3)
+    
+    plt.figure(figsize=(8, 8)) 
+    plt.imshow(image)
+    plt.show()
---
+++ test_center_net.py
@@ -0,0 +1,59 @@
+import numpy as np
+import argparse
+import torch
+from PIL import Image
+
+from src.center_net.utils import decode, center_match, _rescale_dets
+from src.center_net.network import CenterNet
+from src.helpers import display_bbox
+
+def decode_ouputs(outputs: list) -> list:
+    """From outputs of the model, decode the detections and centers
+
+    Args:
+        outputs (list): Outputs of the model
+
+    Returns:
+        list: Decoded detections and centers (bbox, class)
+    """
+    
+    dets, cts = decode(*outputs, 
+                       K=10,
+                       ae_threshold=0.5,
+                       kernel=3,
+                       num_dets=20)
+
+    dets = dets.detach().numpy()
+    cts = cts.detach().numpy()
+
+    borders = np.array([[  0., 300.,   0., 300.]])
+    ratios = np.array([[0.25, 0.25]])
+    sizes = np.array([[300, 300]])
+
+    _rescale_dets(dets, cts, ratios, borders, sizes)
+    return center_match(dets[0], cts[0])
+
+def pred(model, image):
+    model.eval()
+    with torch.no_grad():
+        output = model(image)[0]
+        return decode_ouputs(output)
+            
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image", help="Path to image")
+    
+    args = parser.parse_args()
+    image = np.array(Image.open(args.image), dtype=np.float32)
+    image = image / 255.
+    image = np.expand_dims(image, axis=0)
+
+    model = CenterNet()
+    model.load_state_dict(torch.load("models/center_net_model.pth"))
+    
+    dets, cts = pred(model, torch.from_numpy(image).unsqueeze(0).float())
+    
+    image = image.transpose((1, 2, 0))
+     
+    display_bbox(image, dets)
