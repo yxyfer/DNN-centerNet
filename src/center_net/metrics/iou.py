26ffd1917e40a6de97d601455ec973e171cde282
Mathieu Rivier <mathieu.rivier@epita.fr>
2023-01-18 14:49:21 +0100

    add(IoU) - Finish main IoU implem + tests

--- src/center_net/metrics/iou.py
+++ src/center_net/metrics/iou.py
@@ -1,15 +1,28 @@
+import numpy as np
+
+
 class IoU(object):
     """This is an IoU calculator
 
-    It takes in the standard output y_hat from the program and the truth value 
+    It takes in the standard output y_hat from the program and the truth value
     y_true and perform an IoU (intersection over Union) on the data.
 
-    It pre-processes it to make the data understandable and then performs the 
+    It pre-processes it to make the data understandable and then performs the
     IoU and returns it.
     Args:
         y_true (_type_): _description_
         y_hat  (_type_): _description_
     """
+
+    def get_bounding_box(self, coords):
+        """Returns the fours coords of a bounding box
+
+        Args:
+            coords (np.array): contains the top-left and bottom-right corner
+            points.
+        """
+        x1, y1, x2, y2 = coords
+
     def __init__(self, y_true, y_hat):
         self.y_true = y_true
         self.y_hat = y_hat
@@ -26,20 +39,19 @@ class IoU(object):
     def __ne__(self, other):
         return self._iou != other
 
+    def get_values(self, box):
+        return np.array([[box[0], box[1]], [box[2], box[3]]])
+
     def pre_process(self):
-        pass
+        self.y_hat = self.get_values(self.y_hat)
+        self.y_true = self.get_values(self.y_true)
+
+    def intersection(self):
+        return (self.y_true & self.y_hat).sum((0, 1))
+
+    def union(self):
+        return (self.y_true | self.y_hat).sum((0, 1))
 
     def perform_calculation(self):
-        y_true, y_hat = self.y_true, self.y_hat
-
-        x_1, y_1 = max(y_true[0], y_hat[0]), max(y_true[1], y_hat[1])
-        x_2, y_2 = min(y_true[2], y_hat[2]), min(y_true[3], y_hat[3])
-
-        interection_area = max(0, x_2 - x_1 + 1) * max(0, y_2 - y_1 + 1)
-
-        y_true_area = (y_true[2] - y_true[0] + 1) * (y_true[3] - y_true[1] + 1)
-        y_hat_area = (y_hat[2] - y_hat[0] + 1) * (y_hat[3] - y_hat[1] + 1)
-
-        iou = interection_area / float(y_true_area + y_hat_area - interection_area)
-
-        return iou
+        SMOOTH = 1e-6
+        return (self.intersection() + SMOOTH) / (self.union() + SMOOTH)
