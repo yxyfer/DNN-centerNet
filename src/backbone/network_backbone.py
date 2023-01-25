f34e857cd270587996ef02f7207bad203ba9c8df
Moustapha DIOP <moustephdiop@gmail.com>
2023-01-25 11:50:01 +0100

    (remove) dead code

--- src/backbone/network_backbone.py
+++ src/backbone/network_backbone.py
@@ -1,23 +1,6 @@
 from torch import nn
 from ..helpers import Convolution, Residual
 
-# class BackboneNetwork(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-
-#         self.network = nn.Sequential(
-#             Convolution(1, 32),
-#             nn.MaxPool2d(2),
-#             Convolution(32, 64),
-#             nn.MaxPool2d(2),
-#             nn.Flatten(),
-#             nn.Linear(7 * 7 * 64, 10),
-#         )
-    
-#     def forward(self, x):
-#         return self.network(x)
-
-        
 class BackboneNetwork(nn.Module):
     def __init__(self):
         super().__init__()
