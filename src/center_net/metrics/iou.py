cfa637f8b1b4b6fe63f789c40a4643cf612383f4
Mathieu Rivier <mathieu.rivier@epita.fr>
2023-01-27 18:44:30 +0100

    update(ap) - handle empty y_hat

--- src/center_net/metrics/iou.py
+++ src/center_net/metrics/iou.py
@@ -64,43 +64,47 @@ class AP:
     def __init__(self, min_score: float = 0.1, smooth: float = 1e-6):
         self.min_score = min_score
         self.smooth = smooth
-        
+
     def perform_iou(self, y_true, y_hat):
         inter = (y_true & y_hat).sum((0, 1))
         union = (y_true | y_hat).sum((0, 1))
-    
+
         return (inter + self.smooth) / (union + self.smooth)
-        
+
     def calculate(self, y_true: np.array, y_hat: np.array) -> tuple:
         """Calculate the average precision and false discovery rate
 
         Args:
             y_true (np.array): Ground truth of shape (N, 5) | (tlx, tly, brx, bry, class)
             y_hat (np.array): Calculated values of shape (M, 8) | (tlx, tly, brx, bry, cx, cy, score, class)
-            
+
         Returns:
             tuple: Average precision and false discovery rate
         """
-        
+
+        if not y_hat.size:
+            return 0, 0
+
         y_hat = y_hat[y_hat[:, 6] > self.min_score]
         y_true = y_true[~np.all(y_true == 0, axis=1)]
 
         fd = np.ones(y_hat.shape[0])
         ap = 0
-        
+
         iou = np.zeros((y_true.shape[0], y_hat.shape[0]))
         for i in range(y_true.shape[0]):
             for j in range(y_hat.shape[0]):
                 if y_true[i, 4] == y_hat[j, 7]:
-                    iou[i, j] = self.perform_iou(y_true[i, :4].astype('int32').reshape(1, 4),
-                                                 y_hat[j, :4].astype('int32').reshape(1, 4))
+                    iou[i, j] = self.perform_iou(
+                        y_true[i, :4].astype("int32").reshape(1, 4),
+                        y_hat[j, :4].astype("int32").reshape(1, 4),
+                    )
 
         if not np.any(iou):
             return 0, 0
-        
+
         best_index = np.argmax(iou, axis=1)
         ap = np.mean(iou[np.arange(y_true.shape[0]), best_index])
         fd[best_index] = 0
-        
-        return ap, np.mean(fd) 
-    
+
+        return ap, np.mean(fd)
