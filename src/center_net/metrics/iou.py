a5a5fe800fec364cbe4cb732b16387e68992db60
Mathieu Rivier <mathieu.rivier@epita.fr>
2023-01-29 18:28:59 +0100

    rebase(main)

--- src/center_net/metrics/iou.py
+++ src/center_net/metrics/iou.py
@@ -1,59 +1,9 @@
 import numpy as np
 
-from src.helpers.bbox import get_bbox
-
-
-class IoU(object):
-    """This is an IoU calculator
-
-    It takes in the standard output y_hat from the program and the truth value
-    y_true and perform an IoU (intersection over Union) on the data.
-
-    It pre-processes it to make the data understandable and then performs the
-    IoU and returns it.
-    Args:
-        y_true (_type_): _description_
-        y_hat  (_type_): _description_
-    """
-
-    def __init__(self, y_true, y_hat):
-        self.y_true = y_true
-        self.y_hat = y_hat
-
-        self.pre_process()
-        self._iou = self.perform_calculation()
-
-    def __call__(self):
-        return self._iou
-
-    def __eq__(self, other):
-        return self._iou == other
-
-    def __ne__(self, other):
-        return self._iou != other
-
-    def get_values(self, box):
-        return np.array([get_bbox(box)[:4]])
-
-    def pre_process(self):
-        self.y_hat = self.get_values(self.y_hat)
-        self.y_true = self.get_values(self.y_true)
-
-    def intersection(self):
-        return (self.y_true & self.y_hat).sum((0, 1))
-
-    def union(self):
-        return (self.y_true | self.y_hat).sum((0, 1))
-
-    def perform_calculation(self):
-        SMOOTH = 1e-6
-        return (self.intersection() + SMOOTH) / (self.union() + SMOOTH)
-
 
 class AP:
-    def __init__(self, min_score: float = 0.1, smooth: float = 1e-6):
+    def __init__(self, min_score: float = 0):
         self.min_score = min_score
-        self.smooth = smooth
 
     def perform_iou(self, y_true, y_hat):
         inter = (y_true & y_hat).sum((0, 1))
@@ -123,10 +73,7 @@ class AP:
         for i in range(y_true.shape[0]):
             for j in range(y_hat.shape[0]):
                 if y_true[i, 4] == y_hat[j, 7]:
-                    iou[i, j] = self.perform_iou(
-                        y_true[i, :4].astype("int32").reshape(1, 4),
-                        y_hat[j, :4].astype("int32").reshape(1, 4),
-                    )
+                    iou[i, j] = self.iou(y_true[i, :4], y_hat[j, :4])
 
         if not np.any(iou):
             return 0, {}
