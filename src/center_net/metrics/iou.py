3c3a4e006140f3a2191815452edd1bfa677238c7
Mathieu Rivier <mathieu.rivier@epita.fr>
2023-01-29 18:26:17 +0100

    rebase(main)

--- src/center_net/metrics/iou.py
+++ src/center_net/metrics/iou.py
@@ -26,6 +26,37 @@ class AP:
     ) -> dict:
         """Calculate the average precision and false discovery rate for given thresholds
 
+<<<<<<< HEAD
+=======
+        Returns:
+            float: IoU value
+        """
+
+        intersection_y1 = max(y_true[0], y_pred[0])
+        intersection_x1 = max(y_true[1], y_pred[1])
+        intersection_y2 = min(y_true[2], y_pred[2])
+        intersection_x2 = min(y_true[3], y_pred[3])
+
+        intersection_area = max(intersection_y2 - intersection_y1, 0) * max(
+            intersection_x2 - intersection_x1, 0
+        )
+
+        y_true_area = (y_true[2] - y_true[0]) * (y_true[3] - y_true[1])
+
+        y_pred_area = (y_pred[2] - y_pred[0]) * (y_pred[3] - y_pred[1])
+
+        return intersection_area / float(y_true_area + y_pred_area - intersection_area)
+
+    def calculate_ap_fd(
+        self,
+        y_true: np.array,
+        y_hat: np.array,
+        ious: np.array,
+        thresholds: list = [0.05, 0.5, 0.75, 0.95],
+    ) -> dict:
+        """Calculate the average precision and false discovery rate for a given threshold
+
+>>>>>>> 4618be2 (rebase(main))
         Args:
             y_true (np.array): Ground truth of shape (N, 5) | (tlx, tly, brx, bry, class)
             y_hat (np.array): Calculated values of shape (M, 8) | (tlx, tly, brx, bry, cx, cy, score, class)
@@ -39,14 +70,24 @@ class AP:
         dic = {}
 
         for threshold in thresholds:
+<<<<<<< HEAD
             fd = np.ones(y_hat.shape[0])
 
             iou = np.where(ious > threshold, ious, 0)
+=======
+            fd = np.zeros(y_hat.shape[0])
+            iou = np.where(ious > threshold, 1, 0)
+
+>>>>>>> 4618be2 (rebase(main))
             best_index = np.argmax(iou, axis=1)
 
             y_true
             ap = np.mean(iou[np.arange(y_true.shape[0]), best_index])
+<<<<<<< HEAD
             fd[best_index] = 0
+=======
+            fd[np.where(iou.sum(axis=0) == 0)] = 1
+>>>>>>> 4618be2 (rebase(main))
 
             dic["AP_{}".format(threshold)] = ap
             dic["FD_{}".format(threshold)] = np.mean(fd)
@@ -58,8 +99,14 @@ class AP:
         y_true: np.array,
         y_hat: np.array,
         thresholds: list = [0.05, 0.5, 0.75, 0.95],
+<<<<<<< HEAD
     ) -> tuple:
         """Calculate the average IoU, average precision and false discovery rate
+=======
+        keep_zeros: bool = False,
+    ) -> tuple:
+        """Calculate the average precision and false discovery rate
+>>>>>>> 4618be2 (rebase(main))
 
         Args:
             y_true (np.array): Ground truth of shape (N, 5) | (tlx, tly, brx, bry, class)
@@ -86,7 +133,18 @@ class AP:
         if not np.any(iou):
             return 0, {}
 
+<<<<<<< HEAD
         best_indexes = np.argmax(iou, axis=1)
         avg_iou = np.mean(iou[np.arange(y_true.shape[0]), best_indexes])
 
         return np.mean(avg_iou), self.caculate_ap_fd(y_true, y_hat, iou, thresholds)
+=======
+        best_index = np.argmax(iou, axis=1)
+        m_iou = iou[np.arange(y_true.shape[0]), best_index]
+        if not keep_zeros:
+            m_iou = m_iou[m_iou != 0]
+
+        m_iou = np.mean(m_iou)
+
+        return m_iou, self.calculate_ap_fd(y_true, y_hat, iou, thresholds)
+>>>>>>> 4618be2 (rebase(main))
